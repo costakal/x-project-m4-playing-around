eb34b3560bf4541467b8241608082b1d990a6555
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-07-28 12:50:42 -0400

    Added H2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Here is where Content goes!</h1>
+        <h2>Moire content</h2>
       </header>
     </div>
   );
