f58fb62632bf8253df488ce367acd8a2d0abbdbf
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-07-28 14:00:40 -0400

    added Styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import { users } from "./data/users";
+import Header from "./components/Header";
+import Members from "./components/Members";
 
 function App() {
   return (
     <>
-      <header>
-        <h1>FaceScpace</h1>
-      </header>
+      <Header />
+      <Members />
     </>
   );
 }
---
+++ src/components/Members.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+import { users } from "../data/users";
+
+const Members = () => {
+  return (
+    <>
+      <MembersTitle>All FaceSpace Members</MembersTitle>
+      <ul>
+        {users.map((user) => {
+          return <li>{user.name}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Members;
+
+const MembersTitle = styled.h2`
+  color: orange;
+`;
